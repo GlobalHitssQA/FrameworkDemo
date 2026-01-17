89ada0a407e8f23241c8a676fab5671c446cd89d
Javier Fuentes Mora <javier.fuentesm@hotmail.com>
2026-01-17 15:09:57 +0900

    Add steps: TC98_B2B2C_Package_Creation_in_BSCS_170126

---
+++ steps/TC98_B2B2C_Package_Creation_in_BSCS_170126Steps.js
@@ -0,0 +1,71 @@
+const { Given, When, Then } = require('@cucumber/cucumber');
+const { expect } = require('@playwright/test');
+const BSCS7PackagePage = require('../pages/BSCS7PackagePage');
+
+let packagePage;
+
+Given('the user is authenticated in BSCS7 system with package configuration permissions', async function () {
+  packagePage = new BSCS7PackagePage(this.page);
+  await packagePage.navigateToLogin();
+  await packagePage.login();
+  await packagePage.verifyMainScreenDisplayed();
+});
+
+Given('the SOLD plan is configured in the system', async function () {
+  await packagePage.verifySOLDPlanConfigured();
+});
+
+When('the user navigates to B2B2C package creation module', async function () {
+  await packagePage.navigateToB2B2CPackageModule();
+});
+
+When('the user selects the option to create a new package', async function () {
+  await packagePage.clickCreateNewPackage();
+});
+
+Then('the system displays the package creation form with capacity, cost, validity and classification fields', async function () {
+  const isFormDisplayed = await packagePage.isPackageFormDisplayed();
+  expect(isFormDisplayed).toBeTruthy();
+});
+
+When('the user enters package data with capacity {string} cost {string} validity {string} and classification {string}', async function (capacity, cost, validity, classification) {
+  await packagePage.fillPackageCapacity(capacity);
+  await packagePage.fillPackageCost(cost);
+  await packagePage.fillPackageValidity(validity);
+  await packagePage.selectPackageClassification(classification);
+});
+
+Then('the system validates and accepts the entered data', async function () {
+  const isDataValid = await packagePage.isDataValidationSuccessful();
+  expect(isDataValid).toBeTruthy();
+});
+
+When('the user configures restrictions with local consumption only without roaming and queuing enabled', async function () {
+  await packagePage.selectLocalConsumptionOnly();
+  await packagePage.disableRoaming();
+  await packagePage.enableQueuing();
+});
+
+Then('the system registers coverage and queuing restrictions correctly', async function () {
+  const areRestrictionsApplied = await packagePage.areRestrictionsApplied();
+  expect(areRestrictionsApplied).toBeTruthy();
+});
+
+When('the user saves the B2B2C 120GB package configuration', async function () {
+  await packagePage.savePackageConfiguration();
+});
+
+Then('the system creates the package and displays a successful creation confirmation message', async function () {
+  const confirmationMessage = await packagePage.getConfirmationMessage();
+  expect(confirmationMessage).toContain('exitosa');
+});
+
+When('the user verifies the package in the available packages list for SOLD plan', async function () {
+  await packagePage.navigateToPackageList();
+  await packagePage.filterBySOLDPlan();
+});
+
+Then('the system displays the B2B2C 120GB package with 24 months validity in the catalog with all configured data', async function () {
+  const isPackageVisible = await packagePage.isPackageInCatalog('B2B2C', '120GB', '24');
+  expect(isPackageVisible).toBeTruthy();
+});
