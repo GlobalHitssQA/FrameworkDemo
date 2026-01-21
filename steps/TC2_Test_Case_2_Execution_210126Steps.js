3c117903ba055fc9fbd88da904ce356f893e82b7
Javier Fuentes Mora <javier.fuentesm@hotmail.com>
2026-01-21 16:39:41 -0500

    Add steps: TC2_Test_Case_2_Execution_210126

---
+++ steps/TC2_Test_Case_2_Execution_210126Steps.js
@@ -0,0 +1,19 @@
+const { Given, When, Then } = require('@cucumber/cucumber');
+const { expect } = require('@playwright/test');
+const TestPage = require('../pages/TestPage');
+
+let testPage;
+
+Given('the user is on the application home page', async function () {
+  testPage = new TestPage(this.page);
+  await testPage.navigateToHomePage();
+});
+
+When('the user views the main content', async function () {
+  await testPage.waitForMainContent();
+});
+
+Then('the page should be displayed correctly', async function () {
+  const isVisible = await testPage.isMainContentVisible();
+  expect(isVisible).toBe(true);
+});
