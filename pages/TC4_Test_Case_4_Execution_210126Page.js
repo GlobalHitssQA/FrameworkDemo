63ca86e3d8f76ea37865a445df46ae47832655c9
Javier Fuentes Mora <javier.fuentesm@hotmail.com>
2026-01-21 16:39:46 -0500

    Add page: TC4_Test_Case_4_Execution_210126

---
+++ pages/TC4_Test_Case_4_Execution_210126Page.js
@@ -0,0 +1,33 @@
+class TestPage {
+  constructor(page) {
+    this.page = page;
+    this.baseUrl = 'https://example.com';
+    this.actionButton = '[data-testid="action-button"]';
+    this.resultContainer = '[data-testid="result-container"]';
+    this.submitButton = '#submit-btn';
+    this.mainContent = '[data-testid="main-content"]';
+  }
+
+  async navigate() {
+    await this.page.goto(this.baseUrl);
+    await this.page.waitForLoadState('domcontentloaded');
+  }
+
+  async performAction() {
+    await this.page.click(this.actionButton);
+  }
+
+  async isResultVisible() {
+    return await this.page.isVisible(this.resultContainer);
+  }
+
+  async clickSubmit() {
+    await this.page.click(this.submitButton);
+  }
+
+  async getMainContentText() {
+    return await this.page.textContent(this.mainContent);
+  }
+}
+
+module.exports = TestPage;
